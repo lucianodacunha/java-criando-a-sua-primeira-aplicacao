f083bdd3e50fe1502e673408e48e694b38df977a
Luciano <lucianodacunha.net@gmail.com>
2023-09-17 14:28:30 -0300

    aula 2: Construindo um catálogo de filmes.

--- primeiro-projeto/src/Main.java
+++ primeiro-projeto/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         System.out.println("E ai mundão, firmeza?!");
+
+        int valor = 10;
+        double valorDouble = 30.0;
+        float valorFloat = 20.0f;
+        String palavra = "Java";
+
+        System.out.println("Imprimindo variáveis: " + valor);
+        System.out.printf("Imprimindo variáveis %.2f\n", valorDouble);
+
+        int cast = (int) valorDouble;
+        System.out.println("Valor int: " + cast);
+
+        String lorem = """
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy 
+                text ever since the 1500s, when an unknown printer took a 
+                galley of type and scrambled it to make a type specimen book. 
+                It has survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged. It was 
+                popularised.
+                """;
+
+        System.out.println(lorem);
     }
 }
---
+++ primeiro-projeto/src/io/github/lucianodacunha/converter/Program.java
@@ -0,0 +1,33 @@
+package io.github.lucianodacunha.converter;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Escreva um programa que converta uma temperatura em graus Celsius para
+ * Fahrenheit. Utilize variáveis para representar os valores das temperaturas e
+ * imprima no console o valor convertido de Celsius para Fahrenheit.
+ *
+ * Dica: A fórmula para converter temperaturas de graus Celsius para Fahrenheit
+ * é: (temperatura * 1.8) + 32.
+ *
+ * Depois de finalizar, testar e conferir que seu programa foi executado com
+ * sucesso, crie uma variável inteira para exibir a temperatura em Fahrenheit
+ * sem casas decimais. Lembre-se que provavelmente você precisará fazer um
+ * casting de valores.
+ */
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        System.out.println("Conversor de Temperatura (Celsius para Fahrenheit)");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Entre com a tempoeratura em Celsius: ");
+        double celsiusTemp = input.nextDouble();
+        int fahTemp = (int) converterCelsiusToFah(celsiusTemp);
+        System.out.printf("Celsius: %.2f => Fahrenheit: %d", celsiusTemp, fahTemp);
+    }
+
+    static double converterCelsiusToFah(double celsiusTemp){
+        return (celsiusTemp * 1.8) + 32;
+    }
+}
